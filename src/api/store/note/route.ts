b46c4afd8fbd25ad33556242b0fc3db1a99b0ee5
Md-Aashiqq <md.aashiq.tvl@gmail.com>
2025-05-11 10:01:23 +0530

    second pharse testing

--- src/api/store/note/route.ts
+++ src/api/store/note/route.ts
@@ -6,20 +6,18 @@ import type { MedusaRequest, MedusaResponse } from "@medusajs/framework/http"
 export const POST = async (req: MedusaRequest, res: MedusaResponse) => {
   // You would typically resolve your NoteModuleService here and create a note
   // Example:
-  // const noteModuleService = req.scope.resolve("noteModuleService")
-  // const note = await noteModuleService.createNotes({ content: req.body.content })
-  // res.json({ note })
+  const noteModuleService = req.scope.resolve("note")
+  const note = await noteModuleService.createNotes({ content: req.body.content })
+  res.json({ note })
 
-  res.json({ message: "[POST] Note created!" }) // Placeholder
 }
 
 // GET: Retrieve notes
 export const GET = async (req: MedusaRequest, res: MedusaResponse) => {
   // You would typically resolve your NoteModuleService here and fetch notes
   // Example:
-  // const noteModuleService = req.scope.resolve("noteModuleService")
-  // const notes = await noteModuleService.listNotes()
-  // res.json({ notes })
+    const noteModuleService = req.scope.resolve("note")
+    const notes = await noteModuleService.listNotes()
+    res.json({ notes })
 
-  res.json({ message: "[GET] Notes fetched!" }) // Placeholder
 }
